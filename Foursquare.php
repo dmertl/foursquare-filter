796104f0486fd108c87166f1b5ef0c59915995b8
David Mertl <dave@onzra.com>
2012-08-15 19:24:01 -0700

    Adding caching of API responses

--- Foursquare.php
+++ Foursquare.php
@@ -5,6 +5,8 @@ class Foursquare {
 	protected $accessToken;
 	public $authenticatedUser;
 
+	const CACHE_TTL = 3600;
+
 	public function __construct() {
 		if(!$access_token = $this->getAccessTokenFromSession()) {
 			if(!$access_token = $this->getAccessTokenFromCookie()) {
@@ -77,13 +79,19 @@ class Foursquare {
 	}
 
 	public function apiCall($url_piece, $query_params=array()) {
-		$query_params['oauth_token'] = $this->getAccessToken();
-		$query_params['v'] = Configure::read('Foursquare.ApiVersionDate');
-		$url = 'https://api.foursquare.com/v2/' . $url_piece . '?' . http_build_query($query_params);
-		if($response = file_get_contents($url)) {
-			if($response = json_decode($response)) {
-				return $response;
+		$cache_key = $this->getCacheKey($url_piece, $this->getAccessToken(), $query_params);
+		if(!($response = $this->getCachedApiResponse($cache_key))) {
+			$query_params['oauth_token'] = $this->getAccessToken();
+			$query_params['v'] = Configure::read('Foursquare.ApiVersionDate');
+			$url = 'https://api.foursquare.com/v2/' . $url_piece . '?' . http_build_query($query_params);
+			if($response = file_get_contents($url)) {
+				$this->cacheApiResponse($cache_key, $response);
+			} else {
+				return false;
 			}
+		}
+		if($response = json_decode($response)) {
+			return $response;
 		}
 		return false;
 	}
@@ -137,5 +145,41 @@ class Foursquare {
 			}
 		}
 		return true;
+	}
+
+	/**
+	 * Gets the API response from cache if it is set
+	 * @param string $key The cache key for the API call
+	 * @return bool|string The API response string or false on failure
+	 */
+	public function getCachedApiResponse($key) {
+		if(function_exists('apc_fetch')) {
+			return apc_fetch($key);
+		}
+		return false;
+	}
+
+	/**
+	 * Caches an API response string
+	 * @param string $key The cache key for the API call
+	 * @param string $value The API response string
+	 * @return bool
+	 */
+	public function cacheApiResponse($key, $value) {
+		if(function_exists('apc_store')) {
+			return apc_store($key, $value, self::CACHE_TTL);
+		}
+		return false;
+	}
+
+	/**
+	 * Returns the cache key for an API call
+	 * @param string $url_piece The URL segment provided to apiCall that identifies the endpoint
+	 * @param string $access_token The access token for the currently authenticated user
+	 * @param array $query_params An array of query params for the api call (excluding version and auth token)
+	 * @return string
+	 */
+	public function getCacheKey($url_piece, $access_token, $query_params=array()) {
+		return $url_piece . $access_token . json_encode($query_params);
 	}
 }
